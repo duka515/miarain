3dd939ca49a113c62fcaf6fc3f98969411f987ec
duka515 <duka.jet@gmail.com>
2026-08-30 14:19:39 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,12 +5,12 @@ window.SITE = {
   videos: 214,
   photos: 389,
   avatar: "assets/avatar.jpeg",
-  liveUrl: "https://t.mbjrkmms.com/133986/8780/38462?aff_sub3=TC_LW9H5CT9&aff_sub5=SF_006OG00000NbNlB&aff_sub=TTT",
-  liveImage: "assets/popup-live.jpeg",
-  premiumUrl: "https://t.mbjrkmms.com/133986/8780/38462?aff_sub3=TC_LW9H5CT9&aff_sub5=SF_006OG00000NbNlB&aff_sub=TTT",
-  premiumImage: "assets/popup-premium.jpeg",
-  photosUrl: "https://t.mbjrkmms.com/133986/8780/38462?aff_sub3=TC_LW9H5CT9&aff_sub5=SF_006OG00000NbNlB&aff_sub=TTT",
-  photosImage: "assets/popup-photos.jpeg",
+  liveUrl: "https://t.mbjrkmms.com/133986/8780/38462?aff_sub3=TC_LW9H5CT9&aff_sub5=SF_006OG00000NbNlB&aff_sub=RD1",
+  liveImage: "assets/popup-live.gif",
+  premiumUrl: "https://t.mbjrkmms.com/133986/8780/38462?aff_sub3=TC_LW9H5CT9&aff_sub5=SF_006OG00000NbNlB&aff_sub=RD1",
+  premiumImage: "assets/popup-premium.gif",
+  photosUrl: "https://t.mbjrkmms.com/133986/8780/38462?aff_sub3=TC_LW9H5CT9&aff_sub5=SF_006OG00000NbNlB&aff_sub=RD1",
+  photosImage: "assets/popup-photos.gif",
   popup: {
     enabled: true,
     brand: "Jerkmate",
@@ -20,8 +20,8 @@ window.SITE = {
       "2- Get the Free Lifetime Membership",
       "3- Enjoy thousands of models like me"
     ],
-    image: "assets/popup.svg",
+    image: "assets/popup.gif",
     button: "Join for Free",
-    url: "https://t.mbjrkmms.com/133986/8780/38462?aff_sub3=TC_LW9H5CT9&aff_sub5=SF_006OG00000NbNlB&aff_sub=TTT"
+    url: "https://t.mbjrkmms.com/133986/8780/38462?aff_sub3=TC_LW9H5CT9&aff_sub5=SF_006OG00000NbNlB&aff_sub=RD1"
   }
 };
